8113c0e0176469635bf24e80bbe5e5bf3bdb0d10
Simon Lopez Calderon <simon.lopezcalde@gmail.com>
2023-02-17 11:13:08 -0500

    intento solucion bug  F5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const ioyneRouter = require('./routes/ioyne.routes')
-const cors = require('cors')
+const ioyneRouter = require('./routes/ioyne.routes');
+const cors = require('cors');
+const path = require('path');
 
 
 // starting the server with express in "app" variable
@@ -21,11 +22,18 @@ app.use(ioyneRouter)
 
 app.use((err, req, res, next) => {
   return res.json({
-      message: err.message
+    message: err.message
   })
 })
 
+// Configuracion ruta para permitir f5
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // starting the server on port 4000
 app.listen(4000);
-console.log('server on port 4000') 
+console.log('server on port 4000')
 
